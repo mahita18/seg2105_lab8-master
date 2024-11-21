2915a971a4d32273ea09ab9cdc3927e7110d73ff
mahita <mahita1818@gmail.com>
2024-11-21 14:56:34 -0500

    modify validator

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -7,13 +7,17 @@ public class PasswordValidator {
 
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
+        boolean hasNumber = false;
+        int length = 0;
 
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpperCase = true;
             if (Character.isLowerCase(c)) hasLowerCase = true;
+            if (Character.isDigit(c)) hasNumber = true;
+            length++;
 
             // If both conditions are met, the password is valid
-            if (hasUpperCase && hasLowerCase) return true;
+            if (hasUpperCase && hasLowerCase && hasNumber && (length>=10)) return true;
         }
         return false;
     }
